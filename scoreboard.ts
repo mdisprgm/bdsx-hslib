9b9baaf2aa9dc8ebb68b40592f72584bc40d60dd
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2021-12-02 20:29:19 +0900

    update scoreboard

--- scoreboard.ts
+++ scoreboard.ts
@@ -1,63 +1,38 @@
 import { Actor } from "bdsx/bds/actor";
 import { serverInstance } from "bdsx/bds/server";
-import { Player } from "bdsx/bds/player";
 import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
+import { ScoreboardId } from "bdsx/bds/scoreboard";
 
-export function getScoreByActor(
+const level = serverInstance.minecraft.getLevel();
+const scoreboard = level.getScoreboard();
+
+export function getScoreByActorSync(
     target: Actor,
     objective: string
-): null | number {
-    const level = serverInstance.minecraft.getLevel();
-    const score = level.getScoreboard();
-    const obj = score.getObjective(objective);
+): number | null {
+    const obj = scoreboard.getObjective(objective);
     if (!obj) return null;
 
-    const id = score.getPlayerScoreboardId(target as Player);
-    return obj.getPlayerScore(id).value;
+    let scoreId: ScoreboardId;
+
+    if (target.isPlayer()) {
+        scoreId = scoreboard.getPlayerScoreboardId(target);
+    } else {
+        scoreId = scoreboard.getActorScoreboardId(target);
+    }
+    return obj.getPlayerScore(scoreId).value;
 }
 
-export function getScoreByNetworkId(
-    targetNetworkID: NetworkIdentifier,
+export function getScoreByNetworkIdSync(
+    targetNetId: NetworkIdentifier,
     objective: string
-): null | number {
-    const target = targetNetworkID.getActor();
+): number | null {
+    const target = targetNetId.getActor();
+    if (!target) return null;
 
-    const level = serverInstance.minecraft.getLevel();
-    const score = level.getScoreboard();
-    const obj = score.getObjective(objective);
+    const obj = scoreboard.getObjective(objective);
     if (!obj) return null; //
 
-    const id = score.getPlayerScoreboardId(target as Player);
-    return obj.getPlayerScore(id).value;
+    const scoreId = scoreboard.getPlayerScoreboardId(target);
+    return obj.getPlayerScore(scoreId).value;
 }
-/*
-const level = serverInstance.minecraft.getLevel()!;
-const players = level.players!;
-const countOfPlayers = level.getActivePlayerCount()!;
-
-const interval = setInterval(() => {
-  for (let i = 0; i < countOfPlayers; i++) {
-    if (getScoreByActor(players.get(i), "test__") == 1) {
-      let level = serverInstance.minecraft.getLevel();
-      let score = level.getScoreboard();
-      let id = score.getPlayerScoreboardId(players.get(i));
-      //get Id
-
-      let info = new ScorePacketInfo();
-      info.objectiveName = "test__";
-      info.scoreboardId = id;
-      info.score = 0;
-      info.type = ScorePacketInfo.Type.PLAYER;
-
-      const pkt = SetScorePacket.create();
-      pkt.entries.push(info);
-      pkt.type = SetScorePacket.Type.CHANGE;
-      pkt.sendTo(players.get(i).getNetworkIdentifier());
-      pkt.dispose();
-    }
-  }
-}, 100);
-events.serverStop.on(() =>{
-  clearInterval(interval);
-});
-*/
