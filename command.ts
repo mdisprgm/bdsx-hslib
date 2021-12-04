1422c8427e7322f512fae40af9f40fa4a76ccc69
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2021-12-04 14:35:18 +0900

    add dynamicOutput

--- command.ts
+++ command.ts
@@ -1,6 +1,8 @@
 import { Actor } from "bdsx/bds/actor";
+import { CommandOrigin } from "bdsx/bds/commandorigin";
 import { Player } from "bdsx/bds/player";
 import { bedrockServer } from "bdsx/launcher";
+import { red } from "colors";
 export namespace MCCmd {
     export const run = bedrockServer.executeCommand;
     export const runOnConsole = bedrockServer.executeCommandOnConsole;
@@ -11,10 +13,43 @@ export namespace MCCmd {
 
     export function Feedback(str: string, target: Player | undefined) {
         if (target)
-            run(
-                `tellraw ${target!.getName()} {"rawtext":[{"text":"${str}"}]}`
-            );
+            run(`tellraw ${target!.getName()} {"rawtext":[{"text":"${str}"}]}`);
     }
+
+    /**
+     * @param origin CommandOrigin
+     * @param message string to outputs
+     */
+    export function dynamicOutputSuccess(
+        origin: CommandOrigin,
+        message: string
+    ): void {
+        const actor = origin.getEntity();
+
+        if (actor?.isPlayer()) {
+            actor.sendMessage(message);
+        } else {
+            console.log(message);
+        }
+    }
+
+    /**
+     * @param origin CommandOrigin
+     * @param message string to outputs
+     */
+    export function dynamicOutputError(
+        origin: CommandOrigin,
+        message: string
+    ): void {
+        const actor = origin.getEntity();
+
+        if (actor?.isPlayer()) {
+            actor.sendMessage("§c" + message);
+        } else {
+            console.log(red(message));
+        }
+    }
+
     export function Log(str: string, prefix = "Feedback") {
         console.log(`[${prefix}]`, str.replace(/§(\w{1})|(\d{1})/g, ""));
     }
