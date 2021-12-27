47132cf3ecb4fe2cf6a1deb786720ec6d7f55f25
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2021-12-27 23:11:14 +0900

    Add: entityAttackWithDamage

--- events.ts
+++ events.ts
@@ -1,8 +1,17 @@
+import { UNDNAME_NAME_ONLY } from "bdsx/dbghelp";
 import { procHacker } from "bdsx/bds/proc";
-import { bool_t, void_t } from "bdsx/nativetype";
+import { ProcHacker } from "bdsx/prochacker";
+import { bool_t, void_t, int32_t } from "bdsx/nativetype";
 import { Actor } from "bdsx/bds/actor";
 import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
 import { Event } from "bdsx/eventtarget";
+import { Player } from "bdsx/bds/player";
+
+const hacker = ProcHacker.load(
+    "hacker.ini",
+    ["Actor::calculateAttackDamage", "Mob::swing"],
+    UNDNAME_NAME_ONLY
+);
 
 const onSneakingSwitched = new Event<
     (actor: Actor, sneaking: boolean) => void
@@ -26,3 +35,28 @@ const setSneaking = procHacker.hooking(
         oldSneaks.set(ni, sneaking);
     }
 });
+
+const Actor$calculateAttackDamage = hacker.hooking(
+    "Actor::calculateAttackDamage",
+    int32_t,
+    null,
+    Actor,
+    Actor
+)((self, target) => {
+    const damage = Actor$calculateAttackDamage(self, target);
+    entityAttackWithDamage.fire(
+        new EntityAttackWithDamageEvent(self, target, damage)
+    );
+    return damage;
+});
+class EntityAttackWithDamageEvent {
+    constructor(
+        public attacker: Actor,
+        public victim: Actor,
+        public damage: number
+    ) {}
+}
+export const entityAttackWithDamage = new Event<
+    (event: EntityAttackWithDamageEvent) => void
+    >();
+
