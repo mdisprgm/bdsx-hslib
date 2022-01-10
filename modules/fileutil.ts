bff1c5acaee36b9e13532be87dc8bdd43f3c1cf2
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-10 11:49:57 +0900

    add fileutil

---
+++ modules/fileutil.ts
@@ -0,0 +1,18 @@
+import fs = require("fs");
+import path = require("path");
+
+export namespace FileUtil {
+    export function mkdirSync(path: string) {
+        try {
+            fs.mkdirSync(path);
+        } catch (err) {
+            throw err;
+        }
+    }
+    export function mkdirRecursiveSync(dirpath: string, dirhas?: Set<string>) {
+        if (dirhas != null && dirhas.has(dirpath)) return;
+        if (dirpath === path.dirname(dirpath)) return;
+        mkdirRecursiveSync(path.dirname(dirpath), dirhas);
+        mkdirSync(dirpath);
+    }
+}
