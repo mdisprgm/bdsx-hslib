fb9780a305cc4bb6faeeb3531f00a34b7045217c
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-12 12:51:11 +0900

    add IntervalUtil

---
+++ modules/interval.ts
@@ -0,0 +1,35 @@
+import { events } from "bdsx/event";
+
+export class IntervalUtil {
+    private static readonly entries: IntervalUtil[] = [];
+    static clearAll() {
+        //.clear에서 entries.splice하기 때문에 뒤에서부터 해야함
+        for (let i = this.entries.length - 1; i >= 0; --i) {
+            this.entries[i].clear();
+        }
+    }
+
+    private readonly timeout: NodeJS.Timeout;
+    private set: boolean = false;
+
+    public constructor(func: () => void, delayMS: number) {
+        this.set = true;
+        this.timeout = setInterval(func, delayMS);
+        IntervalUtil.entries.push(this);
+    }
+
+    clear() {
+        this.set = false;
+        const idx = IntervalUtil.entries.indexOf(this);
+        IntervalUtil.entries.splice(idx, 1);
+        clearInterval(this.timeout);
+    }
+
+    isCleared() {
+        return !this.set;
+    }
+}
+
+events.serverLeave.on(() => {
+    IntervalUtil.clearAll();
+});
