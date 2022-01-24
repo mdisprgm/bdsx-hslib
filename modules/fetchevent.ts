e7706088cb7dc26632da2beff3e4117705139724
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-24 21:08:15 +0900

    Add: fetchevent

---
+++ modules/fetchevent.ts
@@ -0,0 +1,44 @@
+import { Actor } from "bdsx/bds/actor";
+import {
+    ActorWildcardCommandSelector,
+    CommandPermissionLevel,
+} from "bdsx/bds/command";
+import { command } from "bdsx/command";
+import { Event } from "bdsx/eventtarget";
+import { CxxString } from "bdsx/nativetype";
+import { MCCmd } from "./command";
+import { IntervalUtil } from "./interval";
+
+export const FETCH_COMMAND = "fetchentities";
+export class EntitiesDetectedEvent {
+    static Entries: Record</**identifier */ string, /**selectors */ string> =
+        {};
+    static New(identifier: string, selectors: string) {
+        this.Entries[identifier] = selectors;
+    }
+    constructor(public identifier: string, public entities: Actor[]) {}
+}
+IntervalUtil.New(() => {
+    for (const id of Object.keys(EntitiesDetectedEvent.Entries)) {
+        const selector = EntitiesDetectedEvent.Entries[id];
+        MCCmd.run(`${FETCH_COMMAND} ${id} ${selector}`);
+    }
+}, 200);
+export const onEntitiesDetected = new Event<
+    (event: EntitiesDetectedEvent) => void
+>();
+command
+    .register(FETCH_COMMAND, FETCH_COMMAND, CommandPermissionLevel.Host)
+    .overload(
+        (p, o, op) => {
+            const entities = p.entities.newResults(o);
+            if (entities.length === 0) return;
+            onEntitiesDetected.fire(
+                new EntitiesDetectedEvent(p.identifier, entities)
+            );
+        },
+        {
+            identifier: CxxString,
+            entities: ActorWildcardCommandSelector,
+        }
+    );
