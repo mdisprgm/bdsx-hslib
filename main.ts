79b3f3253d4ac7b27c22531cf34cd84dce8df82c
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-03-13 19:51:38 +0900

    exports

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-// do nothing
+import { MCCmd } from "./modules/command";
+import { MCEntity } from "./modules/entity";
+import { fsmgmt } from "./modules/fsmgmt";
+import { IntervalUtil } from "./modules/interval";
+import { MCScore } from "./modules/scoreboard";
+import { Sleep } from "./modules/sleep";
+
+export { MCCmd, MCEntity, fsmgmt, IntervalUtil, MCScore, Sleep };
+
