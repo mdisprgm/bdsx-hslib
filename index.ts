cd17f786a1bcc42a279da60b5f02c62a706fcf66
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-03-13 21:30:06 +0900

    remove main.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-import "./modules/matchTargetsName";
+import { MCCmd } from "./modules/command";
+import { MCEntity } from "./modules/entity";
+import { fsmgmt } from "./modules/fsmgmt";
+import { IntervalUtil } from "./modules/interval";
+import { MCScore } from "./modules/scoreboard";
+import { Sleep } from "./modules/sleep";
+
+export { MCCmd, MCEntity, fsmgmt, IntervalUtil, MCScore, Sleep };
--- main.ts
+++
@@ -1,9 +0,0 @@
-import { MCCmd } from "./modules/command";
-import { MCEntity } from "./modules/entity";
-import { fsmgmt } from "./modules/fsmgmt";
-import { IntervalUtil } from "./modules/interval";
-import { MCScore } from "./modules/scoreboard";
-import { Sleep } from "./modules/sleep";
-
-export { MCCmd, MCEntity, fsmgmt, IntervalUtil, MCScore, Sleep };
-
