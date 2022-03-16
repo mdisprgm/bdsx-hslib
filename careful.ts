397bccc5611c9db2f4c67cb45283b78c6dcac266
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-03-16 20:47:49 +0900

    docstring - careful

--- careful.ts
+++ careful.ts
@@ -2,7 +2,7 @@ import { events } from "bdsx/event";
 import { Event } from "bdsx/eventtarget";
 import { bedrockServer } from "bdsx/launcher";
 
-// maybe not useful for third-parties
+/** maybe not useful for third-parties  */
 export const careful = new Event<() => void>();
 
 if (!bedrockServer.isLaunched()) {
