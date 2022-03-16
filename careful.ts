07b385dbc82656a616c3d014ef293d7a03c564e7
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-03-16 22:48:59 +0900

    fix careful

--- careful.ts
+++ careful.ts
@@ -1,14 +1,4 @@
-import { events } from "bdsx/event";
 import { Event } from "bdsx/eventtarget";
-import { bedrockServer } from "bdsx/launcher";
 
 /** maybe not useful for third-parties  */
 export const careful = new Event<() => void>();
-
-if (!bedrockServer.isLaunched()) {
-    events.serverOpen.on(() => {
-        careful.fire();
-    });
-} else {
-    careful.fire();
-}
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import "./careful";
+import { events } from "bdsx/event";
+import { bedrockServer } from "bdsx/launcher";
 import { MCCmd } from "./modules/command";
 import { MCEntity } from "./modules/entity";
 import { fsmgmt } from "./modules/fsmgmt";
@@ -7,3 +8,12 @@ import { MCScore } from "./modules/scoreboard";
 import { Sleep } from "./modules/sleep";
 
 export { MCCmd, MCEntity, fsmgmt, IntervalUtil, MCScore, Sleep };
+
+import careful = require("./careful");
+if (!bedrockServer.isLaunched()) {
+    events.serverOpen.on(() => {
+        careful.careful.fire();
+    });
+} else {
+    careful.careful.fire();
+}
